8c816893122cecd9fb03ee743a7954e890fc0981
MarkusXV <10934984@uvu.edu>
2021-09-14 12:04:05 -0600

    Create M02_Project_Assign1.py

---
+++ Fall2021/M02_Project_Assign1.py
@@ -0,0 +1,38 @@
+print("Hello! Welcome to my first Python Asignment") #Welcome message
+print(" ") #Adds a blank line
+
+print("My name is Peter Sanford") #Prints my name
+print("I'm a student in Dr. Crandall's class: Fall 2021, Section 1") #My class information
+print(" ") #Adds a blank line
+
+prob_1 = 5 + 4
+
+prob_2 = 5 - 2
+
+prob_3 = (3 * 2) * 4
+
+prob_4 = 2 / 25
+
+prob_5 = (3 * 4) / 2
+
+print("Fun Math Problems:") #title for the body
+print("Rolling a 5 and a 4 on a pair of dice adds up to...") #Problem 1 description
+print("   ", prob_1) #Outputs problem 1 answer
+print(" ") #Adds a blank line
+
+print("There is 5 more days to turn an assignment in.\nIf I do it in 2 days, I have how many days to start the next assignment?") #Problem 2 description
+print("   ", prob_2) #Outputs problem 2 answer
+print(" ") #Adds a blank line
+
+print("If I have 3 hens that lay 2 eggs a day, how many eggs will I have in 4 days?")
+print("   ", prob_3) #Outputs problem 3 answer
+print(" ") #Adds a blank line
+
+print ("I have completed 2 out of 25 assignments so far this semester...")
+print("   ", prob_4) #Outputs problem 4 answer
+print(" ") #Adds a blank line
+
+print("I went to the store and first found 3 items I wanted to buy for $4 each.\nAdditionally, they had a .5 off coupon attached...")
+print("   ", prob_5) #Outputs problem 5 answer
+print(" ") #Adds a blank line
+
