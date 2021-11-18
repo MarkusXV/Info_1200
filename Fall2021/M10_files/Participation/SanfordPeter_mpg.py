a9ec0e42ee5a5d66fb3874599deb08f07201d5b9
MarkusXV <10934984@uvu.edu>
2021-11-18 09:56:21 -0700

    Finished M10 Participation

--- Fall2021/M10_files/Participation/SanfordPeter_mpg.py
+++ Fall2021/M10_files/Participation/SanfordPeter_mpg.py
@@ -1,27 +1,31 @@
 #!/usr/bin/env python3
 
-import csv
+import csv # imports the csv module
 
+'''Gets the miles driven from the user'''
 def get_miles_driven():
-    while (miles_driven := float(input("Enter miles driven:\t"))) <= 0:                    
-        print("Entry must be greater than zero. Please try again.\n")       
-    return miles_driven
-          
-def get_gallons_used():
-    while (gallons_used := float(input("Enter gallons of gas:\t"))) <= 0:                    
-        print("Entry must be greater than zero. Please try again.\n")
-    return gallons_used
+    while (miles_driven := float(input("Enter miles driven:\t"))) <= 0: # Gets the number from the user
+        print("Entry must be greater than zero. Please try again.\n") # Prints an error if the number is less than zero
+    return miles_driven # Returns the number entered
 
+'''Gets the amount of gallons used'''
+def get_gallons_used():
+    while (gallons_used := float(input("Enter gallons of gas:\t"))) <= 0: # Gets the number from the user
+        print("Entry must be greater than zero. Please try again.\n") # Prints an error if the number is less than zero
+    return gallons_used # Returns the number entered
+
+'''Writes the trips list to the csv file'''
 def write_trips(trips):
-    with open("trips.csv", "w", newline = "") as file:
+    with open("trips.csv", "w", newline = "") as file: # opens the file and writes to it
         writer = csv.writer(file)
         writer.writerows(trips)
 
+'''Reads the csv file and puts it into the trips list'''
 def read_trips():
-    with open("trips.csv", "r") as file:
+    with open("trips.csv", "r") as file: # opens the file and reads it
         reader = csv.reader(file)
         trips = []
-        for row in reader:
+        for row in reader: # puts each individual list into the trips list
             each_trip = []
             each_trip.append(row[0])
             each_trip.append(row[1])
@@ -29,49 +33,51 @@ def read_trips():
             trips.append(each_trip)
         return trips
         
-
+'''Reads the csv file and then prints each trip in that list'''
 def list_trips():
-    with open("trips.csv", "r") as file:
-        print("Distance\t Gallons\t MPG")
+    with open("trips.csv", "r") as file: # opens the file and reads it
+        print("Distance\t Gallons\t MPG")# prints the header
         reader = csv.reader(file)
-        for row in reader:
+        for row in reader: # puts each individual list into the trips list
             each_trip = []
             each_trip.append(row[0])
             each_trip.append(row[1])
             each_trip.append(row[2])
-            print(each_trip[0] + "\t\t", each_trip[1] + "\t\t", each_trip[2])
+            print(each_trip[0] + "\t\t", each_trip[1] + "\t\t", each_trip[2]) # prints each trip individually
        
 
 def main():
     # display a welcome message
     print("The Miles Per Gallon Program\n")
 
-    trips = read_trips()
-    list_trips()
+    trips = read_trips() # Calls the read trips function
+    list_trips() # calls the list trips function
     print() # adds a blank line
     
 
-    more = "y"
+    more = "y" # Creates a more variable that will let the user keep going or stop the while loop
     while more.lower() == "y":
-        miles_driven = get_miles_driven()
-        gallons_used = get_gallons_used()
+        miles_driven = get_miles_driven() # calls the get miles function to get it from the user
+        gallons_used = get_gallons_used() # calls the get gallons function to get it from the user
                                  
-        mpg = round((miles_driven / gallons_used), 2)
-        print(f"Miles Per Gallon:\t{mpg}\n")
+        mpg = round((miles_driven / gallons_used), 2) # Calculates the mpg from those two inputted values
+        print(f"Miles Per Gallon:\t{mpg}\n") # Prints the miles per gallon
 
+        # Creates a trip list and puts the new data into it
         new_trip = []
         new_trip.append(miles_driven)
         new_trip.append(gallons_used)
         new_trip.append(mpg)
         trips.append(new_trip)
-        
-        write_trips(trips)
-        list_trips()
 
-        more = input("\nMore entries? (y or n): ")
+        write_trips(trips) # Writes the trips list to the csv
+        list_trips() # Lists the trips again
+
+        more = input("\nMore entries? (y or n): ") # asks the user if they want to add another trip
+        print() # Adds a blank line
     
-    print("\nBye!")
+    print("\nBye!") # prints a bye message
 
-if __name__ == "__main__":
+if __name__ == "__main__": # if this is the main module, call the main function
     main()
 
--- Fall2021/M10_files/Participation/SanfordPeter_mpg_write.py
+++ Fall2021/M10_files/Participation/SanfordPeter_mpg_write.py
@@ -1,52 +1,53 @@
 #!/usr/bin/env python3
 
-import csv
+import csv # imports the csv module
 
+'''Gets the miles driven from the user'''
 def get_miles_driven():
-    while (miles_driven := float(input("Enter miles driven:\t"))) <= 0:                    
-        print("Entry must be greater than zero. Please try again.\n")       
-    return miles_driven
-          
+    while (miles_driven := float(input("Enter miles driven:\t"))) <= 0: # Gets the number from the user
+        print("Entry must be greater than zero. Please try again.\n") # Prints an error if the number is less than zero
+    return miles_driven # returns the number entered
+
+'''Gets the amount of gallons used'''
 def get_gallons_used():
-    while (gallons_used := float(input("Enter gallons of gas:\t"))) <= 0:                    
-        print("Entry must be greater than zero. Please try again.\n")
-    return gallons_used
-        
+    while (gallons_used := float(input("Enter gallons of gas:\t"))) <= 0: # Gets the number from the user                 
+        print("Entry must be greater than zero. Please try again.\n") # Prints an error if the number is less than zero
+    return gallons_used # returns the number entered
+
 def main():
     # display a welcome message
     print("The Miles Per Gallon program")
-    print()
+    print() # Adds a blank line
 
-    alltrips = []
+    alltrips = [] # creates a list that we can use later
 
-    more = "y"
+    more = "y" # Creates a more variable that will let the user keep going or stop the while loop
     while more.lower() == "y":
-        miles_driven = get_miles_driven()
-        gallons_used = get_gallons_used()
+        miles_driven = get_miles_driven() # Calls the get miles function to get it from the user
+        gallons_used = get_gallons_used() # Calls the get gallons function to get it from the user
                                  
-        mpg = round((miles_driven / gallons_used), 2)
-        print(f"Miles Driven:\t{miles_driven}\nGallons_used:\t{gallons_used}\n"
-              f"Miles Per Gallon:\t{mpg}")
-        print()
+        mpg = round((miles_driven / gallons_used), 2) # Calculate the mpg from those two inputted values
+        print(f"Miles Per Gallon:\t{mpg}") # prints the Data that they entered as well as the mpg
+        print() # Adds a blank line
 
+        #Creates a trip list and puts the new data into it
         trip = []
         trip.append(miles_driven)
         trip.append(gallons_used)
         trip.append(mpg)
 
-        alltrips.append(trip)
-        print(trip)
-        print(alltrips)
+        alltrips.append(trip)# adds that list we just made to the alltrips list, making it two-dimensional
         
         
-        more = input("More entries? (y or n): ")
+        more = input("More entries? (y or n): ") # asks the user if they want to add another trip
+        print() # Adds a blank line
     
-    with open("trips.csv", "w", newline = "") as file:
+    with open("trips.csv", "w", newline = "") as file: # Writes the alltrips list to the csv file so that the user will have it later
         writer = csv.writer(file)
         writer.writerows(alltrips)
 
-    print("Bye!")
+    print("\nBye!") # prints a bye message
 
-if __name__ == "__main__":
+if __name__ == "__main__": # if this is the main module, call the main function
     main()
 
